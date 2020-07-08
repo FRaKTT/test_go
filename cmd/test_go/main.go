dd7cacb4512994a643bd7c1c1779c10947b91ba6
Fedor <fraktt@yandex.ru>
2020-07-09 00:23:10 +0300

    2

--- cmd/test_go/main.go
+++ cmd/test_go/main.go
@@ -1,7 +1,9 @@
 package main
 
 import "fmt"
+import "github.com/FRaKTT/test_go/internal/app/int-pack"
 
 func main() {
 	fmt.Println("hello test")
+	int_pack.Hello()
 }
---
+++ internal/app/int-pack/int_pack.go
@@ -0,0 +1,7 @@
+package int-pack
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("hello int_pack")
+}
